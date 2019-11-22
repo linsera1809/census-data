00c838be095e5f930bdaa33670866947e733f528
CORP\linser1 <bobby.linse@goodifycharities.org>
2019-11-21 23:24:06 -0500

    Reading contents finally from the urlConnection

--- src/edu/cscc/CensusData.java
+++ src/edu/cscc/CensusData.java
@@ -1,5 +1,10 @@
 package edu.cscc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * @author Bobby Linse
  * Description: Create a Java class with a main method called CensusData. This class will read data from the URL above.
@@ -16,6 +21,50 @@ package edu.cscc;
 
 public class CensusData {
 
+    public static void main(String args[]) {
+
+        String contents = getUrlContents("https://www2.census.gov/topics/genealogy/1990surnames/dist.all.last");
+        System.out.println(contents);
+    }
+
+    public static void serializeData(){
+
+    }
+
+    private static String getUrlContents(String urlString)
+    {
+        StringBuilder content = new StringBuilder();
+
+        // many of these calls can throw exceptions, so i've just
+        // wrapped them all in one try/catch statement.
+        try
+        {
+            // create a url object
+            URL url = new URL(urlString);
+
+            // create a urlconnection object
+            URLConnection urlConnection = url.openConnection();
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }
+
+
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
 
 
 }
